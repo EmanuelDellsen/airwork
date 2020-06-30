076cc134af91fa49cdf9feeb4b8d916076fadb8d
EmanuelDellsen <emanuel.dellsen@gmail.com>
2020-06-30 16:30:20 +0200

    It now creates a marker on clicking the map

--- client/components/ApplicationInfo.vue
+++ client/components/ApplicationInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="application-info" v-show="value">
         <h2>Type of work: </h2>
         <h2>Money you will receive: </h2>
         <h2>Date of work: </h2>
@@ -7,3 +7,20 @@
         <h2>Description of the job: </h2>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'application-info',
+        props: {
+            value: {
+                required: true
+            }
+        },
+        methods: {
+            close() {
+                this.$emit("input", !this.value);
+                console.log("reached close in appInfo")
+            }
+        }
+    };
+</script>
--- client/components/Map.vue
+++ client/components/Map.vue
@@ -2,57 +2,92 @@
   <div>
     <h1>Search bar component here?</h1>
 
-<GmapMap ref="map" :center="{lat:10, lng:10}"
-  :zoom="7"
-  map-type-id="terrain"
-  style="width: 500px; height: 300px" @click="getClickinfo"></GmapMap>
-  <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      ></gmap-marker>
+    <GmapMap ref="map" :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 500px; height: 300px"
+      @click="getClickinfo">
+      <gmap-marker :key="key" v-for="(item, key) in markers" :position="getPosition(item)" :clickable="true"
+        @click="openApplicationInfo()" />
+      <application-info v-model="applicationInfoWindowOpen" v-bind:value="applicationInfoWindowOpen"></application-info>
+
+    </GmapMap>
+
   </div>
 </template>
 
 <script>
 
-import {gmapApi} from 'vue2-google-maps'
+  import { gmapApi } from 'vue2-google-maps'
+  //import GmapCustomMarker from 'vue2-gmap-custom-marker';
+  import ApplicationInfo from './ApplicationInfo.vue'
 
   export default {
+    components: {
+      ApplicationInfo
+    },
     computed: {
       google: gmapApi
+
     },
-      data(){
-        return{
- map: null,
-      markers: [],
-      places: [],
+    data() {
+      return {
+        map: null,
+        markers: [],
+        places: [],
+        currentLocation: null,
+        currentMarkerIndex: null,
+        currentMarker: null,
+        infoWindowPos: null,
+        applicationInfoWindowOpen: false,
+        infoContent: '',
+        infoOptions: {
+          pixelOffset: {
+            width: 20,
+            height: 40
+          }
         }
-     
+
+
       }
-      ,
-		methods: {
-			getClickinfo(location) {
-        this.location = location;
-        addMarker(location)
-        
+
+    }
+    ,
+    methods: {
+      getClickinfo: function (location) {
+        this.currentLocation = location;
+        this.addMarker()
+        console.log(this.currentLocation.latLng.lat())
+        console.log(this.currentLocation.latLng.lng())
+
 
       },
-      addMarker(location) {
-        const marker = {
-          lat: this.location.latLng.lat(),
-          lng: this.location.latLng.lng()
+      addMarker: function () {
+        let marker = {
+          lat: this.currentLocation.latLng.lat(),
+          lng: this.currentLocation.latLng.lng()
         };
-        this.markers.push({ position: marker });
-        this.places.push(this.location);
-        this.center = marker;
+        //this.markers.push({ position: marker });
+        //this.places.push(this.currentLocation);
+        this.markers.push(marker)
+        console.log(this.markers)
+        console.log(marker.lat)
+        console.log("after this.markers")
+
+      },
+      getPosition: function (marker) {
+        console.log("in getposition")
+        console.log(marker)
+        console.log(marker.lat)
+        this.currentMarker = marker;
+        return {
+          lat: marker.lat,
+          lng: marker.lng
+
+        }
+
+      },
+      openApplicationInfo: function () {
+        this.applicationInfoWindowOpen = !this.applicationInfoWindowOpen;
+        console.log(this.applicationInfoWindowOpen)
       }
-		},
-		mounted() {
-			this.$refs.map.$mapPromise.then((map) => {
-      map.panTo({lat: 22.38, lng: 103.80})
-			})
     }
   };
 
