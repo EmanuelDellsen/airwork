f0770ba8908c4d575ddb9101dac958ff64df8b27
EmanuelDellsen <emanuel.dellsen@hotmail.com>
2020-06-30 13:21:05 +0200

    add clickevent to get info on clicking the map

---
+++ client/components/ApplicationInfo.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Type of work: </h2>
+        <h2>Money you will receive: </h2>
+        <h2>Date of work: </h2>
+        <h2>Time of work: </h2>
+        <h2>Description of the job: </h2>
+    </div>
+</template>
