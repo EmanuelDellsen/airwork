994972ecd0330e0db4cdd986c402f79ac884f883
EmanuelDellsen <emanuel.dellsen@gmail.com>
2020-07-07 11:04:29 +0200

    after search the map pans to location of what is searched for

--- client/components/Map.vue
+++ client/components/Map.vue
@@ -2,141 +2,147 @@
   <div>
     <h1>Search bar component here?</h1>
 
-    <GmapMap
-      ref="map"
-      :center="this.center"
-      :zoom="13"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-      @click="getClickinfo"
-    >
-      <gmap-info-window
-        id="info_window"
-        :options="infoOptions"
-        :position="infoWindowPos"
-        :opened="infoWinOpen"
-        @closeclick="infoWinOpen = false"
-      >
+    <GmapMap ref="map" :center="this.center" :zoom="13" map-type-id="terrain" style="width: 500px; height: 300px"
+      @click="getClickinfo">
+      <gmap-info-window id="info_window" :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen"
+        @closeclick="infoWinOpen = false">
         <button type="button" style="background-color: pink;">
           {{ this.infoContent }}
         </button>
         <ApplicationInfo v-bind:marker="currentMarker"></ApplicationInfo>
       </gmap-info-window>
-      <gmap-marker
-        :key="key"
-        v-for="(item, key) in markers"
-        :position="getPosition(item)"
-        :clickable="true"
-        @click="toggleInfoWindow(item, key)"
-      ></gmap-marker>
+      <gmap-marker :key="key" v-for="(item, key) in markers" :position="getPosition(item)" :clickable="true"
+        @click="toggleInfoWindow(item, key)"></gmap-marker>
     </GmapMap>
   </div>
 </template>
 
 <script>
-import { gmapApi } from "vue2-google-maps";
-//import GmapCustomMarker from 'vue2-gmap-custom-marker';
-import ApplicationInfo from "./ApplicationInfo.vue";
+  import { gmapApi } from "vue2-google-maps";
+  //import GmapCustomMarker from 'vue2-gmap-custom-marker';
+  import ApplicationInfo from "./ApplicationInfo.vue";
 
-export default {
-  name: "Map",
-  props: {
-    searchResult: Object,
-  },
-  components: {
-    ApplicationInfo,
-  },
-  computed: {
-    google: gmapApi,
-  },
-  data() {
-    return {
-      map: null,
-      markers: [],
-      places: [],
-      center: { lat: 10, lng: 10 },
-      currentLocation: null,
-      currentMarkerIndex: null,
-      currentMarker: null,
-      infoWindowPos: null,
-      infoWinOpen: false,
-      applicationInfoWindowOpen: false,
-      infoContent: "",
-      infoOptions: {
-        pixelOffset: {
-          width: 0,
-          height: -35,
-        },
-      },
-    };
-  },
-  mounted() {
-    this.geolocate();
-  },
-  methods: {
-    getClickinfo: function(location) {
-      this.currentLocation = location;
-      this.addMarker();
-      console.log(this.currentLocation.latLng.lat());
-      console.log(this.currentLocation.latLng.lng());
+  export default {
+    name: "Map",
+    props: {
+      searchResult: Object,
     },
-    addMarker: function() {
-      let marker = {
-        position: {
-          lat: this.currentLocation.latLng.lat(),
-          lng: this.currentLocation.latLng.lng(),
-        },
-        markerInfo: "Here detailed info will be",
-      };
-      //this.markers.push({ position: marker });
-      //this.places.push(this.currentLocation);
-      this.markers.push(marker);
+    components: {
+      ApplicationInfo,
     },
-    getPosition: function(marker) {
-      this.currentMarker = marker;
-      console.log(this.currentMarker);
+    computed: {
+      google: gmapApi,
+    },
+    watch: {
+      searchResult: function () {
+        console.log("searchResult changed and is now: ")
+        console.log(this.searchResult)
+        this.goToSearchResult(this.searchResult)
 
+      }
+
+    },
+    data() {
       return {
-        lat: this.currentMarker.position.lat,
-        lng: this.currentMarker.position.lng,
+        map: null,
+        markers: [],
+        places: [],
+        center: { lat: 10, lng: 10 },
+        currentLocation: null,
+        currentMarkerIndex: null,
+        currentMarker: null,
+        infoWindowPos: null,
+        infoWinOpen: false,
+        applicationInfoWindowOpen: false,
+        infoContent: "",
+        infoOptions: {
+          pixelOffset: {
+            width: 0,
+            height: -35,
+          },
+        },
       };
     },
-    toggleInfoWindow: function(marker, idx) {
-      console.log(marker);
-      this.infoWindowPos = marker.position;
-      this.infoContent = marker.markerInfo;
-      console.log(this.infoWindowPos);
-
-      //check if its the same marker that was selected if yes toggle
-      if (this.currentMarkerIndex == idx) {
-        this.infoWinOpen = !this.infoWinOpen;
-      }
-      //if different marker set infowindow to open and reset current marker index
-      else {
-        this.infoWinOpen = true;
-        this.currentMarkerIndex = idx;
-      }
+    mounted() {
+      this.geolocate();
     },
-    geolocate: function() {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
+    methods: {
+      getClickinfo: function (location) {
+        this.currentLocation = location;
+        this.addMarker();
+        console.log(this.currentLocation.latLng.lat());
+        console.log(this.currentLocation.latLng.lng());
+      },
+      addMarker: function () {
+        let marker = {
+          position: {
+            lat: this.currentLocation.latLng.lat(),
+            lng: this.currentLocation.latLng.lng(),
+          },
+          markerInfo: "Here detailed info will be",
         };
-      });
-    },
-  },
-};
+        //this.markers.push({ position: marker });
+        //this.places.push(this.currentLocation);
+        this.markers.push(marker);
+      },
+      getPosition: function (marker) {
+        this.currentMarker = marker;
+        console.log(this.currentMarker);
+
+        return {
+          lat: this.currentMarker.position.lat,
+          lng: this.currentMarker.position.lng,
+        };
+      },
+      toggleInfoWindow: function (marker, idx) {
+        console.log(marker);
+        this.infoWindowPos = marker.position;
+        this.infoContent = marker.markerInfo;
+        console.log(this.infoWindowPos);
+
+        //check if its the same marker that was selected if yes toggle
+        if (this.currentMarkerIndex == idx) {
+          this.infoWinOpen = !this.infoWinOpen;
+        }
+        //if different marker set infowindow to open and reset current marker index
+        else {
+          this.infoWinOpen = true;
+          this.currentMarkerIndex = idx;
+        }
+      },
+      geolocate: function () {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+        });
+      },
+      goToSearchResult: function (searchResult) {
+        this.currentLocation = searchResult;
+        console.log(this.currentLocation)
+        console.log("above is current location after search")
+        let currentLat = this.currentLocation.geometry.location.lat()
+        let currentLng = this.currentLocation.geometry.location.lng()
+        console.log(this.currentLocation.geometry.location.lat(), this.currentLocation.geometry.location.lng())
+        console.log(currentLat, currentLng)
+        this.$refs.map.$mapPromise.then((map) => {
+          map.panTo({ lat: currentLat, lng: currentLng })
+        })
+      }
+
+    }
+  }
 </script>
 
 <style>
-.vue-map-container {
-  height: 450px;
-  max-width: 992px;
-  width: 100%;
-}
+  .vue-map-container {
+    height: 450px;
+    max-width: 992px;
+    width: 100%;
+  }
 
-#info_window {
-  position: relative;
-}
+  #info_window {
+    position: relative;
+  }
 </style>
