dafa74cb5e990e46baf77d5e31b5b5dc866496b2
Synoecism <karl.westgardh@gmail.com>
2020-07-07 23:42:36 +0200

    add new state mgmt to mongodb connection

--- server/main.js
+++ server/main.js
@@ -1,17 +1,17 @@
 //Backend based on https://www.digitalocean.com/community/tutorials/nodejs-crud-operations-mongoose-mongodb-atlas
 
 //Get all backend running serices
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 //Set settings
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const PORT = 5000;
 app.use(express.json()); // make sure it will be in json format
 
 //Get all routes
-const workOpportunityRoute = require('./routes/workOpportunityRoute.js');
+const workOpportunityRoute = require("./routes/workOpportunityRoute.js");
 
 //Get keys
 const username = process.env.MONGODB_ATLAS_USERNAME;
@@ -21,15 +21,58 @@ const database = process.env.MONGODB_ATLAS_DATABASE_NAME;
 //Get uri to db
 const uri = `mongodb+srv://${username}:${password}@airwork-cluster.nnhrk.mongodb.net/${database}?retryWrites=true&w=majority`;
 
-//Connect to db
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+/* Connect to MongoDB Atlas Cloud Server
+@ useNewUrlParser : required to be able to parse connection strings
+@ useUnifiedTopology : required for mongoose to be able to monitor server
+@ useFindAndModify : required to be able to use methods update methods such as findByIdAndUpdate
+*/
+function connect() {
+  setTimeout(function() {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      connectTimeoutMS: 10000, //give up initial connection after 10 seconds
+    });
+  }, 10000);
+}
+//call connect
+connect();
 
 //Interface db connection
 const connection = mongoose.connection;
 
 //Listener to notify when db-connection is established
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+connection.on("error", function() {
+  //triggered when an error is found
+  //console.error.bind(console,`- Status: Failed to connected to MongoDB! @ ${new Date().toString()}`)
+  console.log("- Status: Failed to connect to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
+
+  //reconnect
+  connect();
+});
+connection.once("open", function() {
+  //triggered when connection is open (final state)
+  console.log("- Status: Successfully opened connection to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
+});
+connection.on("connecting", function() {
+  console.log("- Status: Connecting to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
+});
+connection.on("connected", function() {
+  console.log("- Status: Connected to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
+});
+connection.on("reconnected", function() {
+  console.log("- Status: Reconnected to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
+});
+connection.on("disconnected", function() {
+  //triggered before error
+  console.log("- Status: Disconnected to MongoDB!");
+  console.log(`  @ ${new Date().toString()}`);
 });
 
 //Introduction rotue
@@ -41,8 +84,8 @@ app.get("/", (req, res) => {
 app.use(workOpportunityRoute);
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
-app.use('/*', function(req, res) {
-  res.status(404).json({ message: 'Not Found'});
+app.use("/*", function(req, res) {
+  res.status(404).json({ message: "Not Found" });
 });
 
 // Start server, set port
--- server/routes/workOpportunityRoute.js
+++ server/routes/workOpportunityRoute.js
@@ -1,36 +1,40 @@
-const express = require('express');
-const Model = require('../models/WorkOpportunityModel');
+const express = require("express");
+const Model = require("../models/WorkOpportunityModel");
 const app = express();
 
 //get all
-app.get('/workopportunity', async (req, res) => {
+app.get("/workopportunity", async (req, res) => {
   const payload = await Model.find({});
   try {
-    res.send(payload);
+    //check if found any document
+    if (payload == null) {
+      res.status(404).json();
+    } else {
+      res.send(payload);
+    }
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
 //get one
-app.get('/workopportunity/:id', async (req, res) => {
+app.get("/workopportunity/:id", async (req, res) => {
   const payload = await Model.findById(req.params.id);
 
-  //check out unhandle exception here
-  //handle when not found
-  if(payload == null){
-    res.status(404).json();
-  }
-
   try {
-    res.send(payload);
+    //check if found any document
+    if (payload == null) {
+      res.status(404).json();
+    } else {
+      res.send(payload);
+    }
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
 //post one
-app.post('/workopportunity', async (req, res) => {
+app.post("/workopportunity", async (req, res) => {
   const new_object = new Model(req.body);
 
   try {
@@ -41,4 +45,21 @@ app.post('/workopportunity', async (req, res) => {
   }
 });
 
-module.exports = app
+//update one
+app.patch("/workopportunity:id", async (req, res) => {
+  try {
+    console.log("hello sir");
+    const payload = Model.findByIdAndUpdate(req.params.id, req.body);
+    //check if found any document
+    if (payload == null) {
+      res.status(404).json();
+    } else {
+      await payload.save();
+      res.send(payload);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = app;
